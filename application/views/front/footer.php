bdc636d7df40ad125affc7e53d705edb2e30b361
auliam03 <aulia.maulida1612@gmail.com>
2023-12-07 06:42:15 +0700

    project uas

---
+++ application/views/front/footer.php
@@ -0,0 +1,19 @@
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <hr>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-6">
+          By <a href="http://localhost/futsal/">Kelompok 3</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+  </body>
+
+  </html>
---
+++ application/views/front/modul/mod_sosmed.php
@@ -0,0 +1,8 @@
+<div class="bs-callout bs-callout-primary"><h4><i class="fa fa-search"></i> Find Us On</h4></div>
+<div class="input-group">
+  <button type="button" class="btn btn-sm btn-primary"><i class="fa fa-instagram" ><a href="https://www.istagram.com/medkit4u_"></a></i></button>&nbsp
+  <button type="button" class="btn btn-sm btn-primary"><i class="fa fa-instagram"><a href="https://www.instagram.com/auliaz03"></a></i></button>&nbsp
+  <button type="button" class="btn btn-sm btn-primary"><i class="fa fa-instagram"><a href="https://www.instagram.com/alsa"></i></button>&nbsp
+  <button type="button" class="btn btn-sm btn-primary"><i class="fa fa-google-plus"></i></button>&nbsp
+  <button type="button" class="btn btn-sm btn-primary"><i class="fa fa-youtube-play"></i></button>&nbsp
+</div>
